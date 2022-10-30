3e2d01b69f2cbcac2e1ea9795ccfb428b2051836
paulnc <paulalexander.nieves@gmail.com>
2022-10-30 16:47:20 -0500

    Fetch data categories and products

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
-console.log ("hola")
+const URL_products = "http://localhost:8010/proxy/categories";
+
+//Getting Categories
+fetch(URL_products)
+.then((data)=>{return data.json()}) 
+.then((completedata)=>{
+  let data1="";
+  completedata.map((element)=>{
+    data1+=`<div class="UnitElement">${element.name}</div>`
+  });
+  document.getElementById("CategoryElement").innerHTML=data1;
+})
+.catch((error)=>{
+  console.log(error);
+})
+
+
+//Getting Products
+
+const URL_categories = "http://localhost:8010/proxy/products";
+fetch(URL_categories)
+.then((data)=>{return data.json()}) 
+.then((completedata)=>{
+  let data2="";
+  completedata.map((element)=>{
+    data2+=`<div class=productElement>
+    <div>${element.name}</div>
+    <img src="${element.url_image}" width= "190" height="170px" alt="Not Found"/>
+    <div> Price: $${element.price}</div>
+    </div>`
+  });
+  document.getElementById("Products").innerHTML=data2;
+})
+.catch((error)=>{
+  console.log(error);
+})
